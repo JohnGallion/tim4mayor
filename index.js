d3685f9940a618c19a2d9c49f21691c2dbc9ab20
John Gallion <johngallion@Johns-MacBook-Air.local>
2023-10-03 22:18:18 -0400

    countdown to election day and voter registration

--- index.js
+++ index.js
@@ -42,46 +42,32 @@ window.addEventListener("scroll", () => {
   }
 });
 
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-  
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 7, 2023 00:00:00").getTime();
 
-function initializeClock(id, endtime) {
-  const clock = document.getElementById(id);
-  const daysSpan = clock.querySelector('.days');
-  const hoursSpan = clock.querySelector('.hours');
-  const minutesSpan = clock.querySelector('.minutes');
-  const secondsSpan = clock.querySelector('.seconds');
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
+  // Get today's date and time
+  var now = new Date().getTime();
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
   }
-
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
-
-const deadline = new Date(Date.parse(new Date()) + 36 * 24 * 60 * 60 * 1000);
-initializeClock('clockdiv', deadline);
+}, 1000);
 
